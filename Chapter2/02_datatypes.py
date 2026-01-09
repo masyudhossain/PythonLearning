575cc82bc9d66c94a4ed3884b68a23e1acda1da8
Masyud Hossain <masyud.hossain2001@gmail.com>
2026-01-09 12:46:21 +0530

    Add initial implementations of variable, data type, operator, type conversion, and input programs

---
+++ Chapter2/02_datatypes.py
@@ -0,0 +1,6 @@
+a = 1 # a is an integer
+b = 5.22 # b is a floating point number
+c = "Masyud" # c is a string
+d = False # d is a boolean variable
+e = None # e is a none type variable
+print(a,b,c,d,e)
---
+++ Chapter2/03_rules_variables.py
@@ -0,0 +1,7 @@
+a = 23
+aaa = 435
+masyud = 34
+sameer = 45
+_samerr = 34
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
